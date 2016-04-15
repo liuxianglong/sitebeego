e4998d0820aceb1663dfdac5da6e1e97f57f3fc1
liuxianglong <liuxianglong@didichuxing.com>
2016-04-15 16:12:48 +0800

    modify

--- controllers/hello.go
+++ controllers/hello.go
@@ -8,7 +8,7 @@ type MainController struct {
 	beego.Controller
 }
 
-func (c *MainController) Get() {
+func (c *MainController) Hello() {
 	c.Data["Website"] = "beego.me1"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
